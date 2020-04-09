01a70d640a9ad564eff594f03ed7718d8fd22eb8
Doouh <ake.giraldo@hotmail.com>
2020-04-09 17:09:24 -0500

    Fix pep8

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,9 +5,9 @@
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
+
 def deploy():
     path = do_pack()
     if path is None:
         return False
     return do_deploy(path)
-
