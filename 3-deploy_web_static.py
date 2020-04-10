578a9cfada2d6d961505eb48fb29b6e30c1f6143
Kevin Adrian Giraldo Valderrama <30760255+Doouh@users.noreply.github.com>
2020-04-10 14:19:38 -0500

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,12 +2,58 @@
 # Script that creates and distributes an archive to your
 # web servers, using the function deploy
 
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
+from fabric.api import local, runs_once, env, put, run
+from os import stat, path
+from datetime import datetime
 
+path = None
+env.hosts = ["104.196.221.76", "18.234.234.222"]
+
+@runs_once
+def do_pack():
+    try:
+        stat("versions")
+    except:
+        local("mkdir versions")
+    try:
+        time = datetime.now().strftime("%Y%m%d%H%M%S")
+        name = "versions/web_static_" + time + ".tgz"
+        print("Packing web_static to " + name)
+        local("tar -cvzf " + name + " web_static")
+        filesize = path.getsize(name)
+        print("web_static packed: " + name + " -> " + str(filesize) + "Bytes")
+        return name
+    except:
+        return None
+
+def do_deploy(archive_path):
+    try:
+        stat(archive_path)
+    except:
+        return False
+    try:
+        name = archive_path.split("/")
+        name = name[1].split(".")
+        name = name[0]
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/{}/".format(name))
+        run("tar -xzf /tmp/{}.tgz -C \
+            /data/web_static/releases/{}/".format(name, name))
+        run("rm /tmp/{}.tgz".format(name))
+        run("mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current".format(name))
+        print("New version deployed!")
+        return True
+    except:
+        return False
 
 def deploy():
-    path = do_pack()
+    global path
+    if path is None:
+        path do_pack()
     if path is None:
         return False
     return do_deploy(path)
