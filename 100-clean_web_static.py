8219e2271f3aa4d0519734370398091de320df44
Doouh <ake.giraldo@hotmail.com>
2020-04-10 17:30:09 -0500

    Fix task 4

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,5 +8,7 @@ env.hosts = ["104.196.221.76", "18.234.234.222"]
 
 def do_clean(number=0):
     number = int(number)
-    local("ls -d -ltr versions/ | tail -n + {} | xargs -d '\n' rm -f --".format(2 if number < 1 else number + 1))
-    run("ls -d -ltr /data/web_static/releases/ | tail -n + {} | xargs -d '\n' rm -f --".format(2 if number < 1 else number + 1))
+    local("ls -d -1tr versions/* | tail -n +{} | \
+          xargs -d '\n' rm -f --".format(2 if number < 1 else number + 1))
+    run("ls -d -1tr /data/web_static/releases/* | tail -n +{} | \
+          xargs -d '\n' rm -rf --".format(2 if number < 1 else number + 1))
