55d638bf26a10c4142af108af55cd3aa07164183
Doouh <ake.giraldo@hotmail.com>
2020-04-21 11:57:52 -0500

    Add dir web_flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNH!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_c(text):
+    text = text.replace("_", " ")
+    return 'C '+text
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def show_python(text="is cool"):
+    text = text.replace("_", " ")
+    return 'Python '+text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def show_int(n):
+    return '%d is a number' % n
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
